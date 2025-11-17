b36663896a9094ea673318bc4bd6de8cb05cefea
shwetavishwakarma00 <shwetavishwkarma04@gmail.com>
2025-11-17 12:11:59 +0530

    blogSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import TrustedBrands from '../components/TrustedBrands'
+import BlogSection from '../components/BlogSection'
 const Home = () => {
   return (
     <div>
       <Hero/>
       <TrustedBrands />
+      <BlogSection />
     </div>
   )
 }
